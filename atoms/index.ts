06de56d85f3007c4b0ffd847f6efaef8fe7b3109
Reyhane Masumi <41986383+ReyhaneMasumi@users.noreply.github.com>
2024-10-08 20:43:34 +0330

    Update index.ts

--- atoms/index.ts
+++ atoms/index.ts
@@ -1,8 +1,8 @@
 import { atom } from 'jotai';
 
 export const videoLinkState = atom(
-  'https://server15700.contentdm.oclc.org/dmwebservices/index.php?q=dmGetStreamingFile/p15700coll2/15.mp4/byte/json'
+  ''
 );
 export const subtitleLinkState = atom(
-  'https://gist.githubusercontent.com/samdutton/ca37f3adaf4e23679957b8083e061177/raw/e19399fbccbc069a2af4266e5120ae6bad62699a/sample.vtt'
+  ''
 );
